9f78e8b8596358de65e9a6e17e16b471a9003316
hiransathsara <sathsarahiran@gmail.com>
2024-01-30 12:24:15 +0530

    My code

--- frontend/src/components/Admin/Verification.js
+++ frontend/src/components/Admin/Verification.js
@@ -1,8 +1,47 @@
 import React from 'react'
 
 const Verification = () => {
+  
   return (
+    <div>
     <div>Verification</div>
+    
+<form>
+        <label for="seller_id">Seller ID:</label>
+        <input type="text" id="seller_id" name="seller_id" required/><br/><br/>
+
+        <label for="full_name">Owner/Full Name:</label>
+        <input type="text" id="full_name" name="full_name" required/><br/><br/>
+
+        <label for="license_no">License No.:</label>
+        <input type="text" id="license_no" name="license_no" required/><br/><br/>
+
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" name="email" required/><br/><br/>
+
+        <label for="phone_no">Phone No.:</label>
+        <input type="tel" id="phone_no" name="phone_no" required/><br/><br/>
+
+        <label for="district">District:</label>
+        <input type="text" id="district" name="district" required/><br/><br/>
+
+        <label for="address">Address:</label>
+        <input type="text" id="address" name="address" required/><br/><br/>
+
+        <label for="landmark">Landmark (Optional):</label>
+        <input type="text" id="landmark" name="landmark"/><br/><br/>
+
+        <label for="business_registration_certificate">Business Registration Certificate:</label>
+        <input type="file" id="business_registration_certificate" name="business_registration_certificate" accept="image/*" required/><br/><br/>
+
+        <label for="current_bill">Current Bill:</label>
+        <input type="file" id="current_bill" name="current_bill" accept="image/*" required/><br/><br/>
+
+        <button type="submit" name="approve">Approve</button>
+        <button type="reset" name="cancel">Cancel</button>
+</form>
+</div>
+
   )
 }
 
--- frontend/src/components/Admin/Verifications.js
+++ frontend/src/components/Admin/Verifications.js
@@ -1,12 +1,45 @@
 import React from 'react'
 import Verification from './Verification'
+import { MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
+
 
 const Verifications = () => {
   return (
-    <div>Verification
-        <Verification/>
-    </div>
+<div>
+<div class="w3-sidebar w3-bar-block" style={{width:'25%'}}>
+  <a href="#" class="w3-bar-item w3-button">DashBoard</a>
+  <a href="#" class="w3-bar-item w3-button">Add Product</a>
+  <a href="#" class="w3-bar-item w3-button">Message</a>
+</div>
+
+
+<div style={{marginLeft:'25%'}}>
+
+
+    <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">Name</th>
+      <th scope="col">Status</th>
+      <th scope="col">Option</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">2023/12/20</th>
+      <td>jayantha</td>
+      <td>Processing</td>
+      <td><button type="button" class="btn btn-outline-dark">Dark</button></td>
+    </tr>
+  </tbody>
+</table>
+
+</div>
+</div>
   )
 }
 
 export default Verifications
+
+
