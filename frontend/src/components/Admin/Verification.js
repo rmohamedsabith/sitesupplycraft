662709ecd49a3e8e47335f3f506f429486fe180c
hiransathsara <sathsarahiran@gmail.com>
2024-02-15 18:39:53 +0530

    new hiran

--- frontend/src/components/Admin/Verification.js
+++ frontend/src/components/Admin/Verification.js
@@ -1,25 +1,29 @@
-import React from 'react'
-import ProfilePicture from '../../images/Untitled-1.png'
-import BRegistration from '../../images/BusinessRegistration.jpg'
-import EBill from '../../images/electricitybill.jpg'
-import './Verification.css'
+import React, { useState } from 'react';
+import ProfilePicture from '../../images/Untitled-1.png';
+import BRegistration from '../../images/BusinessRegistration.jpg';
+import EBill from '../../images/electricitybill.jpg';
+import './Verification.css';
 
 const Verification = () => {
-  
+  const [fullscreenImage, setFullscreenImage] = useState(null);
+
+  // Function to toggle fullscreen image
+  const toggleFullscreenImage = (image) => {
+    setFullscreenImage(image);
+  };
+
   return (
     <center>
-      <hr/>
+      <hr />
       <h2><u>Account Details</u></h2>
       <div className="Details">
-      <hr/>
-          {/* <div className="container"> */}
-            <div className="row">
-              <div className="col">
-                <img src={ProfilePicture} alt="Company profile" width="200" height="300"/>
-      
-              </div>               
-              <div className="col">
-                <form>
+        <hr />
+        <div className="row">
+          <div className="col" onClick={() => toggleFullscreenImage(ProfilePicture)}>
+            <img src={ProfilePicture} alt="Company profile" width="200" height="300" />
+          </div>
+          <div className="col">
+            <form>
                   <label style={{marginBottom:'15px'}}  htmlFor="seller_id">Seller ID:</label><br/>
                   <label style={{marginBottom:'15px'}} htmlFor="full_name">Owner/Full Name:</label><br/>
                   <label style={{marginBottom:'15px'}} htmlFor="license_no">License No.:</label><br/>
@@ -29,9 +33,9 @@ const Verification = () => {
                   <label style={{marginBottom:'15px'}} htmlFor="address">Address:</label><br/>
                   <label style={{marginBottom:'15px'}} htmlFor="landmark">Landmark (Optional):</label><br/>
                 </form>
-              </div>                        
-              <div className="col">   
-                <form className='verification'>
+          </div>
+          <div className="col">
+            <form className='verification'>
                         <input className="verificationInput"  style={{marginBottom:'10px'}} type="text" id="seller_id" name="seller_id" defaultValue={"S01"} required/><br/>
                         <input className="verificationInput" style={{marginBottom:'10px'}} type="text" id="full_name" name="full_name" defaultValue={"Mark Wood"} required/><br/>
                         <input className="verificationInput" style={{marginBottom:'10px'}} type="text" id="license_no" name="license_no" defaultValue={"ID1234567"} required/><br/>
@@ -41,38 +45,33 @@ const Verification = () => {
                         <input className="verificationInput" style={{marginBottom:'10px'}} type="text" id="district" name="district" defaultValue={"Colombo"} required/><br/>
                         <input className="verificationInput" style={{marginBottom:'10px'}} type="text" id="landmark" name="landmark" defaultValue={"01"}/><br/>
                   </form>
-              </div>           
-            </div>
-            <div className="row">
-              <div className="col">
-              <br/><br/><br/>
-                <label htmlFor="business_registration_certificate">Business Registration Certificate:</label><br/><br/>
-                <img src={BRegistration} alt="Company profile" width="200" height="300"/>
-              </div>
-              
-              <div className="col">
-              <br/><br/><br/>
-                <label htmlFor="current_bill">Current Bill:</label><br/><br/>
-                <img src={EBill} alt="Company profile" width="250" height="150"/>
-              </div>
-            
-            </div>
-          {/* </div> */}
-
-
+          </div>
+        </div>
+        <div className="row">
+          <div className="col" onClick={() => toggleFullscreenImage(BRegistration)}>
+            <img src={BRegistration} alt="Business registration certificate" width="200" height="300" />
+          </div>
+          <div className="col" onClick={() => toggleFullscreenImage(EBill)}>
+            <img src={EBill} alt="Current bill" width="250" height="150" />
+          </div>
+        </div>
       </div>
-            <div className="row">
-              <div className="col">
-                  <button className="verificationBtn" type="submit" name="approve">Approve</button>
-              </div>
-              <div className="col">
-                  <button className="verificationBtn" type="reset" name="cancel">Cancel</button> 
-              </div>
-            </div>
+      {/* Display fullscreen image */}
+      {fullscreenImage && (
+        <div className="fullscreen-image-overlay" onClick={() => setFullscreenImage(null)}>
+          <img src={fullscreenImage} alt="Fullscreen" className="fullscreen-image" />
+        </div>
+      )}
+      <div className="row">
+        <div className="col">
+          <button className="verificationBtn" type="submit" name="approve">Approve</button>
+        </div>
+        <div className="col">
+          <button className="verificationBtn" type="reset" name="cancel">Cancel</button>
+        </div>
+      </div>
     </center>
+  );
+};
 
-
-  )
-}
-
-export default Verification
+export default Verification;
--- frontend/src/components/Admin/Verifications.js
+++ frontend/src/components/Admin/Verifications.js
@@ -1,79 +1,77 @@
-import React from 'react'; // Import React
-import { Link } from 'react-router-dom'; // Import Link if you're using it
-import './Verifications.css'
 
-function Verifications() {
-  return (
-<center>
-    <hr/>
-    <h2><u>Processing Account Details</u></h2>
-    <hr/>
-    <div className="tableDetails">
-    <table className="table">
-    <thead>
-      <tr>
-        <th scope="col"><b>Date</b></th>
-        <th scope="col"><b>Name</b></th>
-        <th scope="col"><b>Status</b></th>
-        <th scope="col"><b>Option</b></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td scope="row">2023/08/23</td>
-        <td>Mark</td>
-        <td style={{color:'orange'}}>Processing...</td>
-        <td><div><Link to={'1'} className="buttonDark">View</Link></div></td>
-      </tr>
-      <tr>
-        <td scope="row">2023/08/23</td>
-        <td>Jacob</td>
-        <td style={{color:'orange'}}>Processing...</td>
-        <td><div><Link to={'2'} className="buttonDark">View</Link></div></td>
-      </tr>
-      <tr>
-        <td scope="row">2023/06/23</td>
-        <td>Sahan</td>
-        <td style={{color:'orange'}}>Processing...</td>
-        <td><div><Link to={'3'} className="buttonDark">View</Link></div></td>
-      </tr>
-      <tr>
-        <td scope="row">2023/09/23</td>
-        <td>Supun</td>
-        <td style={{color:'orange'}}>Processing...</td>
-        <td><div><Link to={'4'} className="buttonDark">View</Link></div></td>
-      </tr>
-      <tr>
-        <td scope="row">2023/12/23</td>
-        <td>Milan</td>
-        <td style={{color:'orange'}}>Processing...</td>
-        <td><div><Link to={'5'} className="buttonDark">View</Link></div></td>
-      </tr>
-      <tr>
-        <td scope="row">2023/09/23</td>
-        <td>Supun</td>
-        <td style={{color:'orange'}}>Processing...</td>
-        <td><div><Link to={'4'} className="buttonDark">View</Link></div></td>
-      </tr>
-      <tr>
-        <td scope="row">2023/09/23</td>
-        <td>Supun</td>
-        <td style={{color:'orange'}}>Processing...</td>
-        <td><div><Link to={'4'} className="buttonDark">View</Link></div></td>
-      </tr>
-      <tr>
-        <td scope="row">2023/09/23</td>
-        <td>Supun</td>
-        <td style={{color:'orange'}}>Processing...</td>
-        <td><div><Link to={'4'} className="buttonDark">View</Link></div></td>
-      </tr>
-    </tbody>
-  </table>
-  </div>
-  <hr/>
-  </center>
- 
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import {Table } from 'react-bootstrap'; 
+import { Link } from 'react-router-dom'; 
+import './Verifications.css'
+import React, { useState } from 'react';
+
+function handleClick() {
+  // Function logic here
 }
 
-export default Verifications;
+function Verifications() {  
+  
+  const [keyword,setKeyword]=useState('')
+  const [status, setStatus] = useState(false);
+
+
+  
+  return (  
+    <>  
+    <div className='textsearch'>
+        Search Name: <input type="text"
+        value={keyword}
+        onChange={(e)=>setKeyword(e.target.value)}
+        ></input>
+        <button className='btn2' onClick={handleClick}  >Search</button></div>
+
+    <div className='p-5'>  
+  <Table striped bordered hover>  
+  <thead>  
+    <tr>  
+      <th><b>Date</b></th>  
+      <th><b>Name</b></th>  
+      <th><b>Status</b></th>  
+      <th><b>Option</b></th>  
+    </tr>  
+  </thead>  
+  <tbody>  
+    <tr>  
+      <td>2023/12/11</td>  
+      <td>John</td>  
+      <td>Processing</td>  
+      <td><Link to={'1'} className="buttonDark">View</Link></td>  
+    </tr>  
+    <tr>  
+      <td>2023/12/10</td>  
+      <td>Nuwan</td>  
+      <td>Processing</td>  
+      <td><Link to={'1'} className="buttonDark">View</Link></td> 
+    </tr>  
+    <tr>  
+      <td>2023/12/10</td>  
+      <td>Nuwan</td>  
+      <td>Processing</td>  
+      <td><Link to={'1'} className="buttonDark">View</Link></td> 
+    </tr>  
+    <tr>  
+      <td>2023/12/10</td>  
+      <td>Nuwan</td>  
+      <td>Processing</td>  
+      <td><Link to={'1'} className="buttonDark">View</Link></td> 
+    </tr>  
+    <tr>  
+      <td>2023/12/10</td>  
+      <td>Nuwan</td>  
+      <td>Processing</td>  
+      <td><Link to={'1'} className="buttonDark">View</Link></td> 
+    </tr>  
+   
+  </tbody>  
+</Table>  
+</div>  
+    </>  
+    
+  );  
+}  
+export default Verifications;  
